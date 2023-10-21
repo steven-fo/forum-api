38450d3eb88eb802c7f29e5ea35c877e1bea07be
steven-fo <stevenfo0711@gmail.com>
2023-10-21 12:53:05 +0700

    fix: delete unused function and rename params

--- src/Domains/threads/ThreadsRepository.js
+++ src/Domains/threads/ThreadsRepository.js
@@ -1,9 +1,5 @@
 class ThreadsRepository {
-  async verifyOwner(addedThread) {
-    throw new Error('THREADS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
-
-  async addThread(addedThread) {
+  async addThread(newThread) {
     throw new Error('THREADS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
