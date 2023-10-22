28b10faa17484d22dbb7a198ab372a08bf13bb1d
steven-fo <stevenfo0711@gmail.com>
2023-10-22 16:14:48 +0700

    fix: add date column

--- migrations/1697539369182_create-table-replies.js
+++ migrations/1697539369182_create-table-replies.js
@@ -9,6 +9,10 @@ exports.up = (pgm) => {
       type: 'TEXT',
       notNull: true,
     },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
     owner: {
       type: 'VARCHAR(50)',
       notNull: true,
