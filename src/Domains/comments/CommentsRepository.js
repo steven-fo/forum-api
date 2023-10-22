41f69d6409ff31448ac86169b843aa11f138322d
steven-fo <stevenfo0711@gmail.com>
2023-10-22 15:52:03 +0700

    fix: fix functions

--- src/Domains/comments/CommentsRepository.js
+++ src/Domains/comments/CommentsRepository.js
@@ -3,7 +3,19 @@ class CommentsRepository {
     throw new Error('COMMENTS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
+  async deleteComment(threadId) {
+    throw new Error('COMMENTS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
   async verifyThread(threadId) {
+    throw new Error('COMMENTS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyComment(threadId) {
+    throw new Error('COMMENTS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyCommentOwner(threadId) {
     throw new Error('COMMENTS_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
